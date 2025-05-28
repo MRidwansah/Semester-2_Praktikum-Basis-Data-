00f5c68a39c413289d13c76d8c74cdc1ff1ffb8a
MRidwansah <muhamadridwang6@gmail.com>
2025-05-28 21:45:19 +0700

    Update and rename db_kampus.sql to Modul 8 db_kampus.sql

--- Modul 8 db_kampus.sql
+++ Modul 8 db_kampus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2025 at 03:38 AM
+-- Generation Time: May 27, 2025 at 03:38 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
